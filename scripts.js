34dcbbfbab92d78c741c98671d88ea3e33411001
Stivenson Mejia <st.domp@gmail.com>
2024-04-25 16:27:43 -0500

    Adicion de la ultima seccion + abrir imagenes y links

--- scripts.js
+++ scripts.js
@@ -15,3 +15,36 @@ window.addEventListener('scroll', function() {
     bush3.style.transform = `translateX(${value}px)`;
     title.style.transform = `translateY(${value * 1.1}px)`;
 });
+
+
+
+// Obtener todas las imágenes
+const images = document.querySelectorAll('.idxSec3_imgContainer img');
+// Obtener el overlay y la imagen en pantalla completa
+const overlay = document.getElementById('overlay');
+const fullscreenImg = document.getElementById('fullscreenImg');
+// Obtener el botón de cerrar
+const closeBtn = document.getElementById('closeBtn');
+
+// Función para mostrar la imagen en pantalla completa
+function openFullscreen(imgSrc) {
+    fullscreenImg.src = imgSrc;
+    overlay.style.display = 'flex'; // Mostrar el overlay
+    document.body.style.overflow = 'hidden'; // Para deshabilitar el scroll del body
+}
+
+// Función para cerrar la imagen en pantalla completa
+function closeFullscreen() {
+    overlay.style.display = 'none'; // Ocultar el overlay
+    document.body.style.overflow = 'auto'; // Para habilitar el scroll del body
+}
+
+// Agregar evento click a cada imagen
+images.forEach(image => {
+    image.addEventListener('click', function() {
+        openFullscreen(this.src); // Abrir imagen en pantalla completa al hacer clic en ella
+    });
+});
+
+// Agregar evento click al botón de cerrar
+closeBtn.addEventListener('click', closeFullscreen);
